20c4d4378a19c8404a7ef1b0f4999fdc0e588ae2
João Palma <joaocarlospalma.io@gmail.com>
2024-02-13 02:45:40 +0000

    Fix CLI usage in backupmanager wrapper script to enforce steam user

--- scripts/wrappers/backupmanager_cli.sh
+++ scripts/wrappers/backupmanager_cli.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-# Wrapper for backup manager for cli usage
+# Wrapper for backup manager for CLI usage
 # Makes the usage easier when running the backup manager from the command line
-# Usage is supposes to be 'backup [option] [arguments]'
+# Usage is supposed to be 'backup [option] [arguments]'
 
-backupmanager "$@"
+# Use eval to correctly handle argument passing
+su steam -c "eval backupmanager $*" -- 
