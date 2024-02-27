f7e4d338db61449b45ecc2e4a3b897f0f12195ed
João Palma <joaocarlospalma.io@gmail.com>
2024-02-27 10:48:47 +0000

    Fix player name splitting issue in player_activity_monitor.sh

--- scripts/utils/player_activity_monitor.sh
+++ scripts/utils/player_activity_monitor.sh
@@ -10,16 +10,16 @@ declare -a current_players
 declare -a previous_players
 
 # An issue arises when players have special characters, commas or spaces in their name.
-# The script uses commas to split the player information into separate fields. 
+# The script uses commas to split the player information into separate fields.
 # If a player's name contains a comma, it will be incorrectly split into separate fields.
 # The same issue occurs with spaces when checking for players who have joined or left.
 #
-# To fix this, we assume that the player's name is everything before the last 
-# two commas (which separate the name, player UID, and Steam UID), 
+# To fix this, we assume that the player's name is everything before the last
+# two commas (which separate the name, player UID, and Steam UID),
 # and that the player UID and Steam UID will never contain any commas.
 #
-# To handle names with multibyte characters, we use 'awk'. 
-# 'awk' is used to split the lines on commas and extract the player UID (the second-to-last field) 
+# To handle names with multibyte characters, we use 'awk'.
+# 'awk' is used to split the lines on commas and extract the player UID (the second-to-last field)
 # and the player name (all fields before the last two).
 # This ensures that we always split on the last two commas.
 # This approach correctly handles player names that contain commas, spaces, and multibyte characters.
@@ -48,20 +48,20 @@ compare_players() {
     local left_players=()
 
     # Convert the player lists to arrays of playeruids
-    mapfile -t previous_uids < <(printf "%s\n" "${previous_players[@]}" | awk -F',' '{print $(NF-1)}')
-    mapfile -t current_uids < <(printf "%s\n" "${current_players[@]}" | awk -F',' '{print $(NF-1)}')
+    mapfile -t previous_uids < <(printf "%s\n" "${previous_players[@]}" | awk -F',' '{if (NF>1) print $(NF-1)}')
+    mapfile -t current_uids < <(printf "%s\n" "${current_players[@]}" | awk -F',' '{if (NF>1) print $(NF-1)}')
 
     # Check for players who have left
     for i in "${!previous_uids[@]}"; do
         if ! [[ " ${current_uids[*]} " =~ ${previous_uids[i]} ]]; then
-            left_players+=("$(echo "${previous_players[i]}" | awk -F',' '{for(i=1;i<=NF-2;i++) printf $i" "; print ""}')")
+            left_players+=("$(echo "${previous_players[i]}" | awk -F',' -v OFS=',' '{for(i=1;i<=NF-2;i++) printf $i (i<NF-2?OFS:""); print ""}')")
         fi
     done
 
     # Check for players who have joined
     for i in "${!current_uids[@]}"; do
         if ! [[ " ${previous_uids[*]} " =~ ${current_uids[i]} ]]; then
-            joined_players+=("$(echo "${current_players[i]}" | awk -F',' '{for(i=1;i<=NF-2;i++) printf $i" "; print ""}')")
+            joined_players+=("$(echo "${current_players[i]}" | awk -F',' -v OFS=',' '{for(i=1;i<=NF-2;i++) printf $i (i<NF-2?OFS:""); print ""}')")
         fi
     done
 
@@ -105,7 +105,7 @@ function start_player_activity_monitor() {
     while true; do
         # Get the current player list
         get_current_players
-            
+
         # Compare it with the previous one
         compare_players
 
