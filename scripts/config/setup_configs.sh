4e59a9189e52ef8c747b3fad8256a881a478869e
João Palma <joaocarlospalma.io@gmail.com>
2024-03-04 14:15:28 +0000

    Refactor server Engine configuration setup

--- scripts/config/setup_configs.sh
+++ scripts/config/setup_configs.sh
@@ -10,12 +10,17 @@ source "${SERVER_DIR}"/scripts/config/setup_rcon_yaml.sh
 # Function to setup the server configs
 function setup_configs() {
     if [[ -n ${SERVER_SETTINGS_MODE} ]] && [[ ${SERVER_SETTINGS_MODE,,} == "auto" ]]; then
-        log_warning ">> SERVER_SETTINGS_MODE is set to '${SERVER_SETTINGS_MODE}', using environment variables to configure the server!"
+        log_warning ">> 'SERVER_SETTINGS_MODE' is set to '${SERVER_SETTINGS_MODE}', environment variables used to configure the server!"
         setup_engine_ini
         setup_palworld_settings_ini
     else
-        log_warning ">> SERVER_SETTINGS_MODE is set to '${SERVER_SETTINGS_MODE}', NOT using environment variables to configure the server!"
-        log_warning ">> File ${GAME_SETTINGS_FILE} has to be manually set by user."
+        log_warning ">> 'SERVER_SETTINGS_MODE' is set to '${SERVER_SETTINGS_MODE}', environment variables NOT used to configure the server!"
+
+        # Copy default-config, which comes with SteamCMD to gameserver save location
+        cp "${GAME_ROOT}/DefaultPalWorldSettings.ini" "${GAME_SETTINGS_FILE}"
+
+        log_warning ">> File '${GAME_ENGINE_FILE}' has to be manually set by user."
+        log_warning ">> File '${GAME_SETTINGS_FILE}' has to be manually set by user."
     fi
     setup_rcon_yaml
 }
--- scripts/config/setup_rcon_yaml.sh
+++ scripts/config/setup_rcon_yaml.sh
@@ -5,53 +5,55 @@ source "${SERVER_DIR}"/scripts/utils/logs.sh
 source "${SERVER_DIR}"/scripts/config/utils.sh
 
 
+
+
+# Function to setup the rcon.yaml config file
+#
+# Description:
+#   This function sets up the 'rcon.yaml' config file for the server.
+#   It will always use 'PalWorldSettings.ini' file to configure the 'rcon.yaml' file.
 function setup_rcon_yaml () {
     if [[ -n ${RCON_ENABLED+x} ]] && [ "${RCON_ENABLED,,}" == "true" ] ; then
-        log_info "> RCON is enabled, setting 'rcon.yaml' config file..."
+        log_info "> RCON is enabled"
 
         mkdir -p "$(dirname "${RCON_CONFIG_FILE}")"
         cp "${SERVER_DIR}/scripts/config/templates/rcon.yaml.template" "${RCON_CONFIG_FILE}"
 
-        if [[ "${SERVER_SETTINGS_MODE,,}" == "auto" ]]; then
-            # Use environment variables
-            log_warning ">> Using environment variables to configure 'rcon.yaml' config file."
-            log_info -n "> Admin Password: " && log_base "'██████████'"
-            log_info -n "> RCON Port: " && log_base "'██████████'"
-
-            if [[ -n ${RCON_PORT+x} ]]; then
-                sed -i "s/###RCON_PORT###/$RCON_PORT/" "$RCON_CONFIG_FILE"
-            else
-                log_error ">>> RCON_PORT is not set, please set it for RCON functionality to work!"
-            fi
-            if [[ -n ${ADMIN_PASSWORD+x} ]]; then
-                sed -i "s/###ADMIN_PASSWORD###/$ADMIN_PASSWORD/" "$RCON_CONFIG_FILE"
-            else
-                log_error ">>> ADMIN_PASSWORD is not set, please set it for RCON functionality to work!"
-            fi
-        else
-            log_warning ">> Using file '$GAME_SETTINGS_FILE' to configure..."
+        if [[ -f "${GAME_SETTINGS_FILE}" ]]; then
+            log_warning ">> Using file '${GAME_SETTINGS_FILE}' to configure '${RCON_CONFIG_FILE}'..."
 
             # Use file information
-            admin_password=$(awk -F'AdminPassword="' '{print $2}' "$GAME_SETTINGS_FILE" | awk -F'"' '{print $1}' | tr -d '\n')
-            rcon_port=$(awk -F'RCONPort=' '{print $2}' "$GAME_SETTINGS_FILE" | awk -F',' '{print $1}' | tr -d '\n')
+            admin_password=$(awk -F'AdminPassword="' '{print $2}' "${GAME_SETTINGS_FILE}" | awk -F'"' '{print $1}' | tr -d '\n')
+            rcon_port=$(awk -F'RCONPort=' '{print $2}' "${GAME_SETTINGS_FILE}" | awk -F',' '{print $1}' | tr -d '\n')
+
+            # Check if rcon port is valid
+            if [ "$(is_valid_port "${rcon_port}" "0")" = 0 ]; then
+                log_error ">>> RCON_PORT is not a valid port number, please set a valid port number for RCON functionality to work!"
+                exit 1
+            fi
 
             log_info -n "> Admin Password: " && log_base "'██████████'"
             log_info -n "> RCON Port: " && log_base "'██████████'"
 
             if [[ -n ${rcon_port+x} ]]; then
-                sed -i "s/###RCON_PORT###/$rcon_port/" "$RCON_CONFIG_FILE"
+                sed -i "s/###RCON_PORT###/${rcon_port}/" "${RCON_CONFIG_FILE}"
             else
                 log_error ">>> RCON_PORT is not set in the file, please set it for RCON functionality to work!"
             fi
             if [[ -n ${admin_password+x} ]]; then
-                sed -i "s/###ADMIN_PASSWORD###/$admin_password/" "$RCON_CONFIG_FILE"
+                sed -i "s/###ADMIN_PASSWORD###/${admin_password}/" "${RCON_CONFIG_FILE}"
             else
                 log_error ">>> ADMIN_PASSWORD is not set in the file, please set it for RCON functionality to work!"
             fi
-        fi
 
-        log_success ">>> Finished setting up 'rcon.yaml' config file."
+            log_success ">>> Finished setting up 'rcon.yaml' config file."
+        else
+            log_warning ">> File '${GAME_SETTINGS_FILE}' does not exist."
+            log_warning ">> Restart the server after configuring '${GAME_SETTINGS_FILE}'."
+            log_warning ">> RCON features will not work!"
+        fi
     else
-        log_warning ">> RCON is disabled, skipping 'rcon.yaml' config file!"
+        log_warning ">> RCON is disabled, skipping '${RCON_CONFIG_FILE}' config file!"
+        log_warning ">> RCON features will be disabled!"
     fi
 }
